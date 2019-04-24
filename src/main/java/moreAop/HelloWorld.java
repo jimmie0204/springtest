d76257901674eeab636beede941fa07aa5444fa0
liyanping06 <liyanping06@meituan.com>
2019-04-24 16:22:42 +0800

    静态aop

---
+++ src/main/java/moreAop/HelloWorld.java
@@ -0,0 +1,19 @@
+package moreAop;/**
+ * Created by jimmie on 2018/11/16.
+ */
+
+/**
+ * @author jimmie
+ * @create 2018-11-16 下午2:28
+ */
+
+public class HelloWorld {
+
+    public void sayHello(){
+        System.out.println("hello world !");
+    }
+    public static void main(String args[]){
+        HelloWorld helloWord =new HelloWorld();
+        helloWord.sayHello();
+    }
+}
